1c9a7437d9fb1ac299dfb3c44371d7b76732e33f
fimal <fimal@radware.com>
2025-05-26 11:50:26 +0300

    update grpc collector

--- k6/start_k6.sh
+++ k6/start_k6.sh
@@ -3,5 +3,5 @@
 # Wait for envoy to get ready
 /bin/sleep 3
 # start test
-k6 run -e DURATION=60s -e RATE=1000 /tests/http_case_01.js -e MY_HOST='envoy:9000' -e USER_IP='100.100.100.100' -e CASE=vector -e DOMAIN=kwaf-demo.test > /dev/null 2>&1
+k6 run -e DURATION=300s -e RATE=1000 /tests/http_case_01.js -e MY_HOST='envoy:9000' -e USER_IP='100.100.100.100' -e CASE=vector -e DOMAIN=kwaf-demo.test > /dev/null 2>&1
 
