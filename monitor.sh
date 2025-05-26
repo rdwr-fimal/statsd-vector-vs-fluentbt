09c76c10fc098c2241b9a6cb0c5f47f7fcc46e08
fimal <fimal@radware.com>
2025-05-26 13:13:54 +0300

    update monitor

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# kubectl top nodes --no-headers | awk '{print "{\"name\": \"" $1 "\", \"cpu\": \"" $2 "\", \"memory\": \"" $3 "\"}"}' | jq -s
-# kubectl top pods --no-headers --all-namespaces | awk '{print "{\"namespace\": \"" $1 "\", \"name\": \"" $2 "\", \"cpu\": \"" $3 "\", \"memory\": \"" $4 "\"}"}' | jq -s
 cd "$(dirname "$0")"
 
 
 monitor() {
     while [[ ($(docker inspect -f {{.State.Running}} $LOGAGENT) == true) && ($(docker inspect -f {{.State.Running}} k6) == true) ]]; do
         sleep 2
-        echo "$(date '+%Y-%m-%d %H:%M:%S'), $(docker stats --no-stream --format "{{.Container}}, {{.CPUPerc}}" $LOGAGENT)" 2>&1 | tee -a "$FILE"
+        echo "$(date '+%Y-%m-%d_%H:%M:%S'), $(docker stats --no-stream --format "{{.Container}}, {{.CPUPerc}}" $LOGAGENT | sed 's/%//g')" 2>&1 | tee -a "$FILE"
         sleep 2
     done
 }
@@ -19,6 +17,6 @@ if (($# != 1));then
     usage
 else
     LOGAGENT=$1
-    FILE=results_${LOGAGENT}_$(date +"%H-%M-%S").log
+    FILE=results_statsd_udp_${LOGAGENT}_$(date +"%Y-%m-%d_%H-%M-%S").log
     monitor
 fi
