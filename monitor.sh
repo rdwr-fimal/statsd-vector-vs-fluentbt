70db73a236e119d105160cc136293c94302f962c
fimal <fimal@radware.com>
2025-05-26 13:08:44 +0300

    update monit script

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 monitor() {
     while [[ ($(docker inspect -f {{.State.Running}} $LOGAGENT) == true) && ($(docker inspect -f {{.State.Running}} k6) == true) ]]; do
         sleep 2
-        echo "$(date '+%Y-%m-%d %H:%M:%S'), $(docker stats --no-stream --format "{{.Container}}, {{.CPUPerc}}" $LOGAGENT)" 2>&1 | tee -a "$FILE"
+        echo "$(date '+%Y-%m-%d_%H:%M:%S'), $(docker stats --no-stream --format "{{.Container}}, {{.CPUPerc}}" $LOGAGENT | sed 's/%//g')" 2>&1 | tee -a "$FILE"
         sleep 2
     done
 }
@@ -17,6 +17,6 @@ if (($# != 1));then
     usage
 else
     LOGAGENT=$1
-    FILE=results_${LOGAGENT}_$(date +"%H-%M-%S").log
+    FILE=results_statsd_tcp_${LOGAGENT}_$(date +"%Y-%m-%d_%H-%M-%S").log
     monitor
 fi
